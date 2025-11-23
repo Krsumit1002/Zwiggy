43c898ffe7eab05efd2cb71c02f2d512d5251d29
sumit kumar <krsumit1002@gmail.com>
2025-11-23 18:39:31 +0530

    sixth commit

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,26 @@
+const foodPartnerModel=require("../models/foodpartner.model")
+const jwt=require("jsonwebtoken")
+
+async function authFoodPartnerMiddleware(req,res,next){
+    const token=req.cookies.token 
+    if(!token){
+        return res.status(401).json({
+            message:"Please login first"
+        })
+    } 
+    try{
+        const decoded=jwt.verify(token,process.env.JWT_SECRET)
+
+        const foodParter=await foodPartnerModel.findById(decoded.id)
+        req.foodParter=foodParter
+        next()
+    }catch(err){
+        return res.status(401).json({
+            message:"Invalid token"
+        })
+    }
+}
+
+module.exports={
+    authFoodPartnerMiddleware
+}
