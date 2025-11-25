7d463a23777fe34c39a73cea16a6ac2e94af2c29
sumit kumar <krsumit1002@gmail.com>
2025-11-26 01:01:52 +0530

    seventh commit

--- backend/src/controllers/food.controller.js
+++ backend/src/controllers/food.controller.js
@@ -1,10 +1,21 @@
 const foodModel=require('../models/food.model')
+const storageService=require("../services/storage.services")
+const { v4: uuidv4 } = require('uuid');
 
 async function createFood(req,res){
-    console.log(req.foodPartner)
-    console.log(req.body)
-    console.log(req.file)
-    res.send("food item created")
+    const fileUploadResult=await storageService.uploadFile(req.file.buffer,uuid())
+
+    const foodItem=await foorModed.create({
+        name:req.body.name,
+        description:req.body.description,
+        video:fileUploadResult.url,
+        foodPartnerId:req.foodPartner._id
+    })
+
+    res.status(201).json({
+        message:"food created successfully",
+        food:foodItem
+    })
 }
 
 module.exports={
